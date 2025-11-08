25ec0d63cced201507f8886ad10c16780bdcfc23
Martín Pielvitori <martinpielvitori@gmail.com>
2025-11-08 01:35:45 +0100

    Fixing formats

--- script/KipuBank.s.sol
+++ script/KipuBank.s.sol
@@ -17,9 +17,9 @@ contract KipuBankScript is Script {
     function run() public {
         // Read environment variables for Mainnet
         address uniswapV2Router = vm.parseAddress(vm.envString("UNISWAP_V2_ROUTER_MAINNET"));
-        address usdcAddress     = vm.parseAddress(vm.envString("USDC_MAINNET"));
-        uint256 bankCapUsd      = vm.envUint("BANK_CAP_USD");
-        uint256 withdrawalUsd   = vm.envUint("WITHDRAWAL_LIMIT_USD");
+        address usdcAddress = vm.parseAddress(vm.envString("USDC_MAINNET"));
+        uint256 bankCapUsd = vm.envUint("BANK_CAP_USD");
+        uint256 withdrawalUsd = vm.envUint("WITHDRAWAL_LIMIT_USD");
 
         // Sanity checks ON-CHAIN
         require(uniswapV2Router.code.length > 0, "Router: no code at addr");
@@ -27,7 +27,7 @@ contract KipuBankScript is Script {
 
         // Display configuration
         address factory = IRouter(uniswapV2Router).factory();
-        address weth    = IRouter(uniswapV2Router).WETH();
+        address weth = IRouter(uniswapV2Router).WETH();
         console2.log("Router:", uniswapV2Router);
         console2.log("Factory:", factory);
         console2.log("WETH:", weth);
@@ -38,12 +38,7 @@ contract KipuBankScript is Script {
         // Deploy
         console2.log("Deploying KipuBank...");
         vm.startBroadcast();
-        kipuBank = new KipuBank(
-            withdrawalUsd,
-            bankCapUsd,
-            usdcAddress,
-            uniswapV2Router
-        );
+        kipuBank = new KipuBank(withdrawalUsd, bankCapUsd, usdcAddress, uniswapV2Router);
         vm.stopBroadcast();
         console2.log("KipuBank deployed at:", address(kipuBank));
     }
